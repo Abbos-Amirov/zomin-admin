ec4b5695188b7d3774195e9c2015baf69f61dbce
BekDevIOS <umekhmonboev@gmail.com>
2025-08-06 00:07:26 +0900

    feat: init admin frontend app, setting custom configs

---
+++ src/app/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
+import  HomePage  from './screens/homePage';
+import  ProductsPage  from './screens/productsPage';
+import  OrdersPage  from './screens/ordersPage';
+import  UserPage  from './screens/userPage';
+import  HomeNavbar  from './components/headers/HomeNavbar';
+import  OtherNavbar  from './components/headers/OtherNavbar';
+import  Footer  from './components/footer';
+import HelpPage from './screens/helpPage';
+import '../css/app.css';
+import '../css/navbar.css';
+import '../css/footer.css';
+
+export default function App() {
+  const location = useLocation();
+  console.log('location',location);
+  return (
+    <>
+      {location.pathname === '/' ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+           <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage/>
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+};
